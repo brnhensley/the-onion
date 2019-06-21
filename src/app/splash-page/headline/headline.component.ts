0fe178c9012ed9f1b2dd25b61abeaf172a659d74
Brian Hensley <brnhensley@gmail.com>
2019-06-21 09:35:29 -0700

    add firebase

--- src/app/splash-page/headline/headline.component.ts
+++ src/app/splash-page/headline/headline.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { ArticleService } from '../../article.service';
 
 @Component({
   selector: 'app-headline',
   templateUrl: './headline.component.html',
-  styleUrls: ['./headline.component.css']
+  styleUrls: ['./headline.component.css'],
+  providers: [ArticleService]
 })
 export class HeadlineComponent implements OnInit {
+  articles: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private articleService: ArticleService) {
+    this.articles.getArticles();
+
+    this.articles.subscribe( items => {
+      console.log(items)
+    })
+
+   }
 
   ngOnInit() {
   }
