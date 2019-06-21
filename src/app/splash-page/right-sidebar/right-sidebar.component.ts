83044bcfcd0979253190d7097e34c7b8dd2681ce
Brian Hensley <brnhensley@gmail.com>
2019-06-21 15:01:18 -0700

    right sidebar working

--- src/app/splash-page/right-sidebar/right-sidebar.component.ts
+++ src/app/splash-page/right-sidebar/right-sidebar.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { ArticleService } from '../../article.service';
 
 @Component({
   selector: 'app-right-sidebar',
   templateUrl: './right-sidebar.component.html',
-  styleUrls: ['./right-sidebar.component.css']
+  styleUrls: ['./right-sidebar.component.css'],
+  providers: [ArticleService]
 })
 export class RightSidebarComponent implements OnInit {
+  articles: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private articleService: ArticleService) {
+    this.articles = articleService.getArticles();
+
+    this.articles.subscribe(items => {
+      console.log(items);
+    });
+  }
 
   ngOnInit() {
   }
